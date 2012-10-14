90117529b22ff636fa4a927e112d7a9f961e02da
Jordan Suchow <suchow@fas.harvard.edu>
2012-10-13 22:47:49 -0400

    formatting changes

--- PlotDegreeDistribution.m
+++ PlotDegreeDistribution.m
@@ -1,11 +1,15 @@
 function PlotDegreeDistribution(graphType, N, varargin)
-  numIterations = 10;
+  numIterations = 1;
   if(nargin < 2)
     N = 1000;
   end
   data = zeros(numIterations,N);
   parfor i = 1:numIterations
-      w = MakeAdjacencyMatrix(graphType,N,varargin{:})
-      data(i,:) = sort(full(sum(w>0)),'descend');
+    fprintf('%d\n', i)
+    w = MakeAdjacencyMatrix(graphType,N,varargin{:})
+    data(i,:) = histc(full(sum(w>0)), [1:N]);
   end
-  loglog([1:N], mean(data), '.k')
+  loglog([1:N], mean(data,1)./N, '.k')
+  makepalettable
+  xlabel('Degree')
+  ylabel('Probability')
