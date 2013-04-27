4c46b8de196e7c251689fa4915b59810edf7620c
Jordan Suchow <suchow@fas.harvard.edu>
2013-04-27 00:08:30 -0400

    pretty print history

---
+++ DrawProcessHistory.m
@@ -0,0 +1,47 @@
+function DrawProcessHistory(history)
+  numSteps = length(history)-1;
+  allTypes = unique(history{1}.tags);
+  numTypes = length(allTypes);
+  colors = palettablecolors(numTypes);
+  initialN = history{1}.N;
+  xscale = 0.15;
+  yscale = 0.135;
+  markerSize = 20;
+  fig = figure('Visible', 'off', 'PaperSize', [initialN*xscale,numSteps*yscale]);
+  for i = 1:(numSteps+1)
+    N = history{i}.N;
+    % draw the population
+    for j = 1:N
+      thisTag = history{i}.tags(j);
+      if(thisTag == -1)
+        color = [0.8,0.8,0.8];
+      else
+        thisTypeNumber = find(allTypes == history{i}.tags(j));
+        color = colors(thisTypeNumber,:);
+      end
+      plot(j, 1-i, '.', 'MarkerSize',markerSize,'Color',color);
+      hold on;
+    end
+    % draw all deaths
+    death = history{i}.death;
+    for j = 1:length(death)
+      plot(death(j), 1-i, '.', 'MarkerSize',markerSize-(markerSize*0.5),'Color',[1,1,1]);
+    end
+    % draw all reproductions
+    reproduce = history{i}.reproduce;
+    for j = 1:length(reproduce)
+      plot(reproduce(j), 1-i, '+', 'MarkerSize',2.5,'Color',[1,1,1]);
+    end
+  end
+  grid off
+  
+  % pretty things up
+  makepalettable
+  set(gca,'Visible','off')
+  
+  % output
+  set(gcf, 'PaperPositionMode', 'manual');
+  set(fig,'PaperPosition',[0 0 initialN*xscale numSteps*yscale])
+  print(fig,'-dpdf','test')
+  
+end
