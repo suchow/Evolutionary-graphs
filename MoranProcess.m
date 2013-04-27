d3bdbdb21f190b9d0547072fb157245dbfccac56
Jordan Suchow <suchow@fas.harvard.edu>
2013-04-27 00:07:37 -0400

    moran and thresholded moran processes

---
+++ MoranProcess.m
@@ -0,0 +1,31 @@
+function history = MoranProcess(N,K,numSteps,graphType)
+  
+  % default graph is complete
+  if(nargin < 4)
+    graphType = 'Complete';
+  end
+  
+  % initialize the population
+  population = MakePopulation(N,graphType{1},graphType{2:end});
+  population = Endow(population,K,true);
+  
+  % run the process
+  for stepIndex = 1:(numSteps+1)
+    
+    % select an individual with probability proportional to fitness
+    i = Select(population,0);
+    
+    % look at the selected individual's outgoing edges, and choose
+    % another individual who this individual's offspring will replace
+    j = find(logical(mnrnd(1,full(population.graph(i,:)))));
+
+    % record the history
+    history{stepIndex} = population; 
+    history{stepIndex}.death = j;
+    history{stepIndex}.reproduce = i;
+    
+    % replace the individual
+    population.tags(j) = population.tags(i);
+    population.memories(j,:) = population.memories(i,:);
+  end
+end
