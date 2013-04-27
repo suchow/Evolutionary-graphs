258fd2c3a7400c0d9964e895b50c5edede5a08ae
Jordan Suchow <suchow@fas.harvard.edu>
2013-04-27 00:08:11 -0400

    basic simulation

--- simulations/simulation1.m
+++ simulations/simulation1.m
@@ -1,45 +1,39 @@
 %
-% Simuates a neutral memory process on various graph structures.
+% Simuates a neutral memory process on various graph structures, plotting
+% the forgetting function and fidelity function at various set sizes.
 %
 function simulation1
-
-  % population settings
+  
   N = 64;
-  Ks = [1,2,3,6,12];
-  numSteps = 2000;
-  graphTypes = {'Complete', 'Square lattice 4', 'Square lattice 8', 'Scale free'} 
-  %graphTypes = {'Complete', 'Random', 'Square lattice 4', 'Path', ...
-  %    'Cycle', 'Scale free', 'Burst', 'Star', 'Integrator', ...
-  %    'Watts-Strogatz', 'Konigsberg', 'Clique wheel'};
+  Ks = [1,2,3,4,6,8,12];
+  numSteps = 250;
+  numIterations = 256;
+  threshold = 1;
 
-  %graphTypes = {'Complete', 'Random', 'Scale free'}
-  %graphTypes = {'Bethe lattice', 'Scale free'}
-
-  % simulation settings
-  numIterations = 32;
-
+  graphTypes = {'Complete', 'Square lattice 4', {'Visual',N,1}, 'Scale free'} 
+  
   %
   % Run the simulation
   %
+  figure;
   for q = 1:length(graphTypes)
+    if(~iscell(graphTypes{q}))
+      thisGraphType = {graphTypes{q}};
+    else
+      thisGraphType = graphTypes{q};
+    end
+        
     for kIndex = 1:length(Ks);
       K = Ks(kIndex);
-      numRemembered = zeros(numSteps,numIterations);
-      numSurvivingLineages = zeros(numSteps,numIterations);
       for j = 1:numIterations
-          population = MakePopulation(N,graphTypes{q});
-          population = Endow(population,K);
-
-          for i = 1:numSteps+1
-              numRemembered(i,j) = length(unique(population.tags));
-              numSurvivingLineages(i,j) = length(unique(population.memories(:,2)));
-              population = Step(population);
-          end
+        history{j} = ThresholdedMoranProcess(N,K,numSteps,thisGraphType,threshold);
+        % compute stats for this iteration
+        [numRemembered(:,j), precision(:,j)] = GetForgettingFunction(history{j}); 
       end
         
-      fprintf('%s\n', graphTypes{q})
-      fprintf('events: %2.2f, lineages: %2.2f\n\n', mean(numRemembered(end,:)), ...
-                        mean(numSurvivingLineages(end,:)))                          
+      fprintf('%s\n', thisGraphType{1})
+      fprintf('events: %2.2f, precision: %2.2f\n\n', mean(numRemembered(end,:)), ...
+                        mean(precision(end,:)))                          
                   
       colors = palettablecolors(length(Ks));
 
@@ -49,25 +43,20 @@ function simulation1
           'LineWidth', 1.5, 'LineSmoothing', 'off');
       hold on;
       axis([1 numSteps 0 max(Ks)])
-      title(graphTypes{q}, 'FontSize', 16);
+      title(graphTypes{q}, 'FontSize', 10);
       if(q == 1) ylabel('Number of remembered events'); end
       makepalettable
 
       % plot timecourse of 
       subplot(2,length(graphTypes),length(graphTypes)+q)
-      plot(mean(numSurvivingLineages'), 'Color', colors(kIndex,:), ...
+      plot(mean(precision'), 'Color', colors(kIndex,:), ...
           'LineWidth', 1.5, 'LineSmoothing', 'off');
       hold on;
       drawnow
       makepalettable
       if(q == 1) ylabel('Number of unique observations'); end
-      axis([1 numSteps 0 N])
+      axis([1 numSteps 0 100])
       xlabel('Number of steps')
     end
   end
 end
-
-% function y = unique(x)
-%     x = sort(x);
-%     y = x([true;diff(x(:))>0]);
-% end
