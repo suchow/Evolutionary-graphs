4fb5dd65e809136564b21828fd74c787c9cc8b16
Jordan Suchow <suchow@post.harvard.edu>
2013-09-20 17:48:56 -0400

    Comment.

--- helpers/whererare.m
+++ helpers/whererare.m
@@ -1,11 +1,11 @@
+% Returns a logical array whose ith element is true iff the ith element of lst
+% is represented fewer than m times. In other words, it finds the rare elements
+% lst, with m as the criterion definition of rarity.
 function loc = whererare(lst, m)
 	loc = zeros(size(lst));
 	unq = unique(lst);
 	for i = 1:length(unq)
 	  matches = (lst==unq(i));
-    % size(matches)
-    % size(loc)
-    % '--'
 	  if(sum(matches) < m)
 	    loc = loc + matches;
     end
